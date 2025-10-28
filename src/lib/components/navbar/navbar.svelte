0e400f95051059da4f36cc48d94a30f5909303de
arovi <arrooficr7@gmail.com>
2025-10-28 21:55:15 +0700

    feat: add Navbar component and integrate with main page
    - Updated package.json to include new dependencies: @internationalized/date and bits-ui.
    - Replaced default content in +page.svelte with Navbar component.
    - Created Navbar component with menu items and avatar integration.
    - Implemented Avatar component with fallback and image handling.
    - Added button and checkbox components with styling and functionality.
    - Developed hover card components for enhanced UI interactions.
    - Introduced navigation menu components for structured navigation.
    - Added textarea component for user input with styling.

---
+++ src/lib/components/navbar/navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Eye, Palette, Undo2, Redo2 } from '@lucide/svelte';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+
+	// list menu items
+	const itemMenu = [
+		{ label: Palette, href: '/profile', info: 'Tema' },
+		{ label: Eye, href: '/settings', info: 'Preview' },
+		{ label: Undo2, href: '/logout', info: 'Undo' },
+		{ label: Redo2, href: '/logout', info: 'Redo' },
+		{ label: 'Publikasikan', href: '/logout', info: 'Simpan' }
+	];
+</script>
+
+<div class="flex h-28 w-full items-start justify-end-safe gap-4 bg-white px-4 pt-5 pr-12 outline-1">
+	{#each itemMenu as item}
+		<HoverCard.Root>
+			<HoverCard.Trigger class="">
+				<Button
+					variant="outline"
+					class="cursor-pointer {item.label == 'Publikasikan'
+						? 'bg-blue-500 text-white '
+						: ''}   hover:bg-gray-100"
+				>
+					{#if typeof item.label === 'string'}
+						<span class="font-serif text-[17px]">{item.label}</span>
+					{:else}
+						<svelte:component this={item.label} class="text-[17px] " />
+					{/if}
+				</Button>
+			</HoverCard.Trigger>
+			<HoverCard.Content
+				class=" flex w-auto items-center justify-center rounded-[4px] bg-gray-500 p-1 pr-3 pl-3 text-center font-sans text-[11px] text-white  "
+				>{item.info}</HoverCard.Content
+			>
+		</HoverCard.Root>
+	{/each}
+
+	<!-- avatar -->
+	<Avatar.Root class="h-10 w-10">
+		<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+		<Avatar.Fallback>CN</Avatar.Fallback>
+	</Avatar.Root>
+</div>
